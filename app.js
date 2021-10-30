fa5b022bed82aafe6b8e0bed804796032a86963c
Portia Greene <pd12300@yahoo.com>
2021-10-30 17:59:35 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const btn1 = document.getElementById(`button1`);
+btn1.onclick = () => {
+    alert(`Cool! You can hear/understand a hummingbird and ants`);
+  }
+
+
+  const h3 = document.querySelector(`h3);
+
+  h3.addEventListener(`mouseenter`, ()=> {
+    alert('OH NO! The squirrels are on to you. Now you have to leave this reality and move to a new one');
+  });
+
+
+
+  const Form = document.querySelector(`form`);
+
+  Form.addEventListener(`submit`, () => {
+    alert(`${The form has been submitted}`);
+  }); 
